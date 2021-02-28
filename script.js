0b3e4f7bf753e5362135d703f02f9832316c6ffc
Stoyann <stoyann.openclassrooms@gmail.com>
2021-02-28 23:52:30 +1100

    animation de la navbar en js

--- script.js
+++ script.js
@@ -3,17 +3,41 @@ const modaleEquipement = document.querySelector(".bloc-modale");
 const imgIndex =document.querySelector(".bloc-modale img");
 
 
-
-btns.forEach(btn => {
-    btn.addEventListener('click', (e) => {
-        imgIndex.src = `ressources/img${e.target.getAttribute('data-index')}-equip.jpg`
-    
-    modaleEquipement.classList.add('active-modale');
+if(window.matchMedia("(min-width: 850px)").matches){
+    btns.forEach(btn => {
+        btn.addEventListener('click', (e) => {
+            imgIndex.src = `ressources/img${e.target.getAttribute('data-index')}-equip.jpg`
+        
+        modaleEquipement.classList.add('active-modale');
+        })
     })
+
+    modaleEquipement.addEventListener("click", () => {
+        modaleEquipement.classList.remove('active-modale');
+    })
+    
+
+}
+
+
+// anim navbar 
+const nav = document.querySelector("nav");
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY > 30 ) {
+        nav.classList.add('anim-nav');
+    } else {    
+        nav.classList.remove('anim-nav');
+
+    }
 })
 
 
 
-modaleEquipement.addEventListener("click", () => {
-    modaleEquipement.classList.remove('active-modale');
-})
+
+
+
+
+
+
+
